11aa8c94dd1459761ed650ec171df34c4f6d6b3d
MaryamMalahnejad <maryam.malahnejad.c@gmail.com>
2024-01-30 21:39:08 +0100

    Change GUI2

--- LA--AdaptiveLearning-Frontend/app.py
+++
@@ -1,7 +0,0 @@
-import dash
-app = dash.Dash(__name__, suppress_callback_exceptions = True, 
-    title = 'AdaptiveLearning', 
-    meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
-    
-)
-
--- pages/analysis.py
+++ pages/analysis.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import urllib.parse
 import plotly.express as px
